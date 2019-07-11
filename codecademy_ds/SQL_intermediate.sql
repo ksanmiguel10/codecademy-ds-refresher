1de622661497193bc4fa92d49ea4f840c0f8fd55
Kathryn Salts <kathrynsalts@gmail.com>
2019-07-11 13:26:58 -0500

    project 2

--- codecademy_ds/SQL_intermediate.sql
+++ codecademy_ds/SQL_intermediate.sql
@@ -49,4 +49,57 @@ FROM startups
 GROUP BY 2
 HAVING AVG(employees) > 500;
 
+-- Hacker News Project
+
+ SELECT title, score
+ FROM hacker_news
+ ORDER BY score DESC
+ LIMIT 5;
+ 
+ SELECT SUM(score)
+ FROM hacker_news;
+ 
+ SELECT user, SUM(score)
+ FROM hacker_news
+ GROUP BY user
+ HAVING SUM(score) > 200
+ ORDER BY 2 DESC;
+ 
+ SELECT (517 + 309 + 304 + 282) / 6366.0;
+ 
+ SELECT user, COUNT(*)
+ FROM hacker_news
+ WHERE url LIKE '%https://www.youtube.com/watch?v=dQw4w9WgXcQ%'
+ GROUP BY user
+ ORDER BY COUNT(*) DESC;
+ 
+ SELECT CASE
+   WHEN url LIKE '%github.com%' THEN 'GitHub'
+   WHEN url LIKE '%medium.com%' THEN 'Medium'
+   WHEN url LIKE '%nytimes.com%' THEN 'NY Times'
+   ELSE 'Other'
+  END AS 'Source',
+  COUNT(*)
+FROM hacker_news
+GROUP BY 1;
+
+SELECT timestamp
+FROM hacker_news
+LIMIT 10;
+
+SELECT timestamp,
+   strftime('%H', timestamp)
+FROM hacker_news
+GROUP BY 1
+LIMIT 20;
+
+SELECT strftime('%H', timestamp) AS 'Hour',
+    ROUND(AVG(score), 2) AS 'Score', 
+    COUNT(*) AS 'Count'
+FROM hacker_news
+WHERE timestamp IS NOT NULL
+GROUP BY 1
+ORDER BY 1;
+
+
 
