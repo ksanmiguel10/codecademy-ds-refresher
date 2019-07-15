f3b936dac26981279f368d7844c6a6ee5f8a0df3
Kathryn Salts <kathrynsalts@gmail.com>
2019-07-15 14:23:59 -0500

    rideshare project

--- codecademy_ds/SQL_intermediate.sql
+++ codecademy_ds/SQL_intermediate.sql
@@ -138,6 +138,49 @@ OR medium LIKE '%silver%'
 GROUP BY 2
 ORDER BY 1 DESC;
 
+-- Rideshare Tables
+
+SELECT * FROM trips;
+
+SELECT * FROM riders;
+
+SELECT * FROM cars;
+
+SELECT *
+FROM riders
+CROSS JOIN cars;
+
+SELECT *
+FROM trips
+LEFT JOIN riders
+ON trips.rider_id = riders.id;
+
+SELECT *
+FROM trips
+JOIN cars
+ON trips.car_id = cars.id;
+
+SELECT *
+FROM riders
+UNION
+SELECT *
+FROM riders2;
+
+SELECT AVG(cost)
+FROM trips;
+
+SELECT *
+FROM riders
+WHERE total_trips < 500;
+
+SELECT COUNT(*)
+FROM cars
+WHERE status = 'active';
+
+SELECT *
+FROM cars
+ORDER BY trips_completed DESC
+LIMIT 2;
 
 
 
